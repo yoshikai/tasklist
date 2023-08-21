5ded7c2e2070a87240650e5564c37a540f1bf23c
yoshikai <yoshikai.tsuyoshi@gmail.com>
2023-08-21 14:08:29 +0900

    [修正内容] ・ダミーテーブルのボタンレイアウトを修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tasklist(
     id VARCHAR(8)   PRIMARY KEY,
-    task VARCHAR(8),
+    task VARCHAR(256),
     deadline VARCHAR(10),
     memo VARCHAR(255),
     done BOOLEAN
